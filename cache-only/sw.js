42bf61d27407388fe6e6e1de95127adb3d828028
DezkaReid <elmaildeldezkareid@gmail.com>
2018-08-02 16:31:54 -0500

    Cache only sample

---
+++ cache-only/sw.js
@@ -0,0 +1,46 @@
+/* global self, caches */
+const CACHE_NAME = 'cache-v1'
+const CACHE_ELEMENTS = [
+  '/',
+  '/index.html',
+  '/index.js'
+]
+
+function installCache (cacheName, cacheElements) {
+  return caches.open(cacheName)
+    .then(cache => cache.addAll(cacheElements))
+}
+
+function dropOldCache (actualCache) {
+  return caches.keys()
+    .then(cacheKeys => {
+      return Promise.all(
+        cacheKeys
+          .filter(key => key !== actualCache)
+          .map(cacheName => {
+            console.log('cache-name', cacheName)
+            caches.delete(cacheName)
+          })
+      )
+    })
+}
+
+function cacheOnlyStrategy (event) {
+  return caches.match(event.request)
+}
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(installCache(CACHE_NAME, CACHE_ELEMENTS))
+})
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(dropOldCache(CACHE_NAME))
+})
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(cacheOnlyStrategy(event))
+})
+
+self.addEventListener('error', (e) => {
+  console.error('something happend', e)
+})
